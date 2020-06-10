185686a61b79be8193e6d39464be8202bc7139ad
Brandon Corey <bcorey85@gmail.com>
2020-06-10 13:56:10 -0500

    test push

--- server.js
+++ server.js
@@ -1,42 +1,55 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
-const pg = require('pg')
-const knex = require('knex')
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const pg = require('pg');
+const knex = require('knex');
 
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    }
+	client: 'pg',
+	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
+	}
 });
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => { res.send('its working!') })
+app.get('/', (req, res) => {
+	res.send('its working!');
+});
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => {
+	signin.handleSignin(req, res, db, bcrypt);
+});
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)} )
+app.post('/register', (req, res) => {
+	register.handleRegister(req, res, db, bcrypt);
+});
 
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfileGet(req, res, db);
+});
 
-app.put('/image', (req, res) => { image.handleImageDetect(req, res, db)})
+app.put('/image', (req, res) => {
+	image.handleImageDetect(req, res, db);
+});
 
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.post('/imageurl', (req, res) => {
+	image.handleApiCall(req, res);
+});
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.listen(port || 3000, () => {
-    console.log(`server started on ${port}`)
-})
+	console.log(`server started on ${port}`);
+});
 
+// test
